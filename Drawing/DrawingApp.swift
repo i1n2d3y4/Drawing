696cd149db596b9960a81887661256c8ea15fd12
VeeBee <vikas@triamis.com>
2023-07-17 18:22:53 +0800

    Initial Commit

---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Vikas Bhandari on 17/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
